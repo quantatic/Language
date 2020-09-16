664ffd1a58f3037a581ca58a16695c1c290cab86
Aidan Beggs <nadiasggeb001@gmail.com>
2020-09-16 11:49:04 -0400

    Refactoring DFA/NFA logic from scratch.

--- src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod dfa;
-mod nfa;
-
-pub use dfa::Dfa;
-pub use nfa::Nfa;
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,2 @@
-use language::{Dfa, Nfa};
-
 fn main() {
-    let mut nfa = Nfa::new();
-
-    nfa.add_state(0);
-    nfa.add_state(1);
-    nfa.add_state(2);
-    nfa.add_state(3);
-
-    nfa.add_transition(&0, &1, Some('a'));
-    nfa.add_transition(&0, &2, Some('a'));
-    nfa.add_transition(&1, &2, Some('a'));
-
-    nfa.set_start_state(&0);
-	nfa.add_accepting_state(&2);
-
-    let dfa = nfa.into_dfa();
-	println!("{}", dfa.try_accept("a".chars()));
-	println!("{}", dfa.try_accept("aaa".chars()));
 }
